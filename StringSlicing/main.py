444d470ad78ce0ed767a82ef5ca3cf604f50039e
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-08 18:56:23 +0530

    Add files via upload

---
+++ StringSlicing/main.py
@@ -0,0 +1,16 @@
+name = "Anuj, kanada"
+print(name[0:5])
+print(len(name))
+
+fruit = "Mango"
+len1 = len(fruit)
+print("mango is a", len1, "letter word." )
+
+#Slicing
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5]) 
+print(fruit[0:])
+print(fruit[:])
