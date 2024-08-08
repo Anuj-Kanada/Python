b91bdf093ae6bc45ae9dbbdcfda31be50a10cd01
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-08 18:55:38 +0530

    Add files via upload

---
+++ UserInput/main.py
@@ -0,0 +1,17 @@
+# Simple Example
+a = input()
+print(a)
+
+
+b = input("Enter Your Number: ")
+print("Entered Number is :", b)
+
+
+x = input("Enter first number:")
+y = input("Enter second number: ")
+print(x + y) # it will concate the string
+
+#TyprCasting
+x = input("Enter first number:")
+y = input("Enter second number: ")
+print(int(x) + int(y))
