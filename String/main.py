59a9eec8a82282f810639ddde28ce8c650981813
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-24 12:00:48 +0530

    Add files via upload

---
+++ String/main.py
@@ -0,0 +1,33 @@
+name = "Anuj"
+
+print("Hello, " + name)
+
+apple = "He said, \"I want to eat an apple\" " #escape charcter
+print(apple)
+
+mango = 'He said, I want to eat an mangoes" '
+print(mango)
+
+a = """I am Devil
+    i am thor
+    freefcec
+    cefrfrfr"""
+
+print(a)
+
+str = '''fefdcdsc
+    dewdefefed
+    ewdewedewed
+    dewedewdwe'''
+print(str)
+
+print(name[0]) 
+print(name[1])
+print(name[2])
+print(name[3])
+
+print("Lets use for loop \n")
+
+for character in apple:
+    print (character)
+
