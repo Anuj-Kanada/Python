11356f1f46eab8d58797a3fe670270538c4fad7f
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-24 11:59:39 +0530

    Add files via upload

---
+++ Operators/operators.py
@@ -0,0 +1,26 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
+
+""""Create a calculator capable of performing addition, subtraction, multiplication 
+    and division operations on two numbers. Your program should format the output in
+    a readable manner!"""
+
+a = 10
+b = 5
+
+ans1 = a + b
+print("Addition of",a,"and",b,"is", ans1)
+
+ans2 = a - b
+print("Subtraction of",a,"and",b, "is", ans2)
+
+ans3 = a * b
+print("Multiplication of", a, "and",b, "is :", ans3)
+
+ans4 = a / b
+print("Division of" ,a, "and" ,b, "is :", ans4)
