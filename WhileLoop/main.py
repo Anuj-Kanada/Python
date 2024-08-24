9ea98181e24667c315bc5bbbbb961b2c1acfee4b
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-24 12:03:08 +0530

    Add files via upload

---
+++ WhileLoop/main.py
@@ -0,0 +1,31 @@
+# while loop is not use with number most of time but it use with complex condition
+
+i = 0
+while(i <= 3):
+    print(i)
+    i = i + 1
+
+
+a = int(input("Enter a Number"))
+while(a <= 9):
+    a = int(input("Enter The number"))
+    print(a)
+
+#Decreamenting Loop
+ 
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
+
+""" do while loop does not support in python
+do while loop eecute minimum one time irrespectfully 
+wheather condition is right  or not
+
+#in other launguage
+
+do{
+    #loop body
+}while(condition) """
