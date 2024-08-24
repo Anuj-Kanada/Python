8b12f5563288415de4745fb79726be7a22f6f430
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-24 12:02:50 +0530

    Add files via upload

---
+++ ForLoop/main.py
@@ -0,0 +1,75 @@
+# loop for String or Variable
+name = "Anuj"
+for i in name:
+    print(i)
+
+name1 = "Kanada"
+for a in name1:
+    print(a)
+    if(a == "K"):
+        print("Hello Anuj")
+
+# Loops for the Lists
+colors = ["Red", "Green", "Blue", "Yello"]
+for color in colors:
+    print(color)
+    for b in color: # here if we use color instead of b the output will be same  
+        print(b)
+
+for k in range(5):
+    print(k)      # o/p 0 1 2 3 4
+
+for k in range(5):
+    print(k + 1)  # o/p 1 2 3 4 5
+
+for k in range(5):
+    print(k + 2)  # o/p 2 3 4 5 6
+
+for k in range(1, 25):
+    print(k)        # o/p 1 to 24
+
+for k in range(1, 25):
+    print(k+1)        # o/p 2 to 25
+
+for k in range(1, 12, 3):
+    print(k)
+
+
+for k in range(1, 10, 1):
+    print(k)
+
+
+for k in range(1, 10, 2):
+    print(k)
+
+
+for k in range(1, 10, 3):
+    print(k)
+
+
+for k in range(1, 10, 4):
+    print(k)
+
+
+for k in range(1, 10, 5):
+    print(k)
+
+
+for k in range(1, 10, 7):
+    print(k)
+
+
+for k in range(1, 10, 6):
+    print(k)
+
+
+for k in range(1, 10, 8):
+    print(k)
+
+
+for k in range(1, 10, 9):
+    print(k)
+
+
+for k in range(1, 10, 10):
+    print(k)
