5bede713b1037413e023148de31e88701f7f37c7
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-08-24 12:01:48 +0530

    Add files via upload

---
+++ IfElseConditionalStatement/main.py
@@ -0,0 +1,42 @@
+# a = int(input("Enter your age"))
+# print ("Your age is:",a)
+
+# if(a > 18):
+#     print("You can Drive")
+# else:
+#     print("You can't Drive")
+
+# num = int(input("Enter a Number:"))
+# if (num < 0):
+#     print("number is Negative")
+# elif(num == 0):
+#     print("Number is Zero")
+# elif(num == 10):
+#     print("number is 10" )
+# else:
+#     print("Number is Positive")
+
+
+no = 19
+if(no < 0):
+    print("Number Is Negative")
+elif(no > 0):
+    if(no <= 10):
+        print("Number is between 1-10")
+    elif(no > 10 and no <= 20):
+        print("Number is between 11-20")
+    else:
+        print("number is greater than 20")
+else:
+    print("Number is zero")
+
+
+# #conditonal Operator
+# # >, <, <=, >=, ==, !=
+# b = int(input("Enter Number you want to check"))
+# print(b<20)
+# print(b>20)
+# print(b<=50)
+# print(b>=77)
+# print(b==20)
+# print(b!=20)
